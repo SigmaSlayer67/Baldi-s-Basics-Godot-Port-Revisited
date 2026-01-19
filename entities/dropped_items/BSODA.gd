bf9d312a08d4c20fedf85ac6f44e8474bd5d6b6d
me <terrype001@gmail.com>
2026-01-19 20:48:19 -0300

    Script now statically typed: BSoda, commited separately because it might be important when an error occurs

--- entities/dropped_items/BSODA.gd
+++ entities/dropped_items/BSODA.gd
@@ -1,17 +1,17 @@
 extends Area3D
 
-var npcList = []
-@export var speed = 20.0
-var lifeTime = 30.0
-@onready var soda = $SODA
+var npcList : Array = []
+@export var speed : float = 20.0
+var lifeTime : float = 30.0
+@onready var soda : MeshInstance3D = $SODA
 
 
-func _ready():
+func _ready() -> void:
 	# 1 looks better with viewport
 	if get_tree().root.content_scale_mode == get_tree().root.CONTENT_SCALE_MODE_VIEWPORT:
 		soda.mesh.material["shader_parameter/tile_scale"] = 1
 
-func _physics_process(delta):
+func _physics_process(delta : float) -> void:
 	translate(Vector3.FORWARD*delta*speed) # move forward
 	
 	if lifeTime > 0:
@@ -21,20 +21,20 @@ func _physics_process(delta):
 	
 	for i in npcList: # shift other npcs
 		if i.get("velocity") != null: # check that velocity exists
-			var setVelocity = -global_basis.z*speed
+			var setVelocity : Vector3 = -global_basis.z*speed
 			# set to position then move
 			if i is CharacterBody3D:
-				var collide = i.move_and_collide(setVelocity * delta,true)
+				var collide : KinematicCollision3D = i.move_and_collide(setVelocity * delta,true)
 				if collide:
 					setVelocity = setVelocity.slide(collide.get_normal()).normalized()*setVelocity.length()
 			i.velocity = Vector3(setVelocity.x,i.velocity.y,setVelocity.z)
 			if i.get("navSkipSafe") != null:
 				i.navSkipSafe = true
 
-func _on_body_entered(body):
+func _on_body_entered(body : Node3D) -> void:
 	if body == self: return
 	npcList.append(body)
 
-func _on_body_exited(body):
+func _on_body_exited(body : Node3D) -> void:
 	if npcList.has(body):
 		npcList.erase(body) # remove npc (if they're on the list)
