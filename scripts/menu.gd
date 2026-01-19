802ac918060484bff193541898e01d80c20e2d99
me <terrype001@gmail.com>
2026-01-19 18:52:09 -0300

    scripts statically typed w/ minor adjustments: menu and options

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -3,79 +3,79 @@ extends Control
 var loadingStatus : int
 var progress : Array[float]
 
-func _ready():
+func _ready() -> void:
 	Options.closed.connect(_on_options_back_pressed)
 	Global.unlock_mouse()
 
-func _on_menu_pressed():
+func _on_menu_pressed() -> void:
 	$Title.hide()
 	$Menu.show()
 
-func _on_back_pressed():
+func _on_back_pressed() -> void:
 	$Menu.hide()
 	$Title.show()
 
 
-func _on_how_play_pressed():
+func _on_how_play_pressed() -> void:
 	$Menu.hide()
 	$HowToPlay.show()
 
-func _on_how_back_pressed():
+func _on_how_back_pressed() -> void:
 	$Menu.show()
 	$HowToPlay.hide()
 
 
-func _on_options_pressed():
+func _on_options_pressed() -> void:
 	$Menu.hide()
 	Options.show()
 
-func _on_options_back_pressed():
+func _on_options_back_pressed() -> void:
 	$Menu.show()
 
-func _on_credits_pressed():
+func _on_credits_pressed() -> void:
 	$Credits.show()
 	$Menu.hide()
 	
 
-func _on_credits_back_pressed():
+func _on_credits_back_pressed() -> void:
 	$Credits.hide()
 	$Menu.show()
 
 
-func _on_play_back_pressed():
+func _on_play_back_pressed() -> void:
 	$PlayMenu.hide()
 	$Title.show()
 
 
-func _on_start_pressed():
+func _on_start_pressed() -> void:
 	$PlayMenu.show()
 	$Title.hide()
 
 
-func _on_story_pressed():
+func _on_story_pressed() -> void:
 	$PlayMenu.hide()
 	$Loading.show()
 	Global.endless = false
 	load_scene()
 
 
-func _on_endless_pressed():
+func _on_endless_pressed() -> void:
 	$PlayMenu.hide()
 	$Loading.show()
 	Global.endless = true
 	load_scene()
 
-func load_scene():
-	var scene = "res://scenes/school_house.tscn"
-	ResourceLoader.load_threaded_request(scene)
+func load_scene() -> void:
+	var scene_path : String = "res://scenes/school_house.tscn"
+	ResourceLoader.load_threaded_request(scene_path)
 	while true:
-		loadingStatus = ResourceLoader.load_threaded_get_status(scene, progress)
+		loadingStatus = ResourceLoader.load_threaded_get_status(scene_path, progress)
 		match  loadingStatus:
 			ResourceLoader.THREAD_LOAD_LOADED:
-				get_tree().change_scene_to_packed(ResourceLoader.load_threaded_get(scene))
+				get_tree().change_scene_to_packed(ResourceLoader.load_threaded_get(scene_path))
 				break
 			ResourceLoader.THREAD_LOAD_FAILED:
-				printerr("Error. Could not load Resource: "+str(scene))
+				printerr("Could not load Resource: "+str(scene_path))
 				break
 		await get_tree().process_frame
 	
--- settings/options.gd
+++ settings/options.gd
@@ -2,40 +2,40 @@ extends CanvasLayer
 
 signal closed
 
-func _ready():
+func _ready() -> void:
 	Global.load_settings()
 	$Options/Menu/Sensativity.value = Global.sensativity
 	$Options/Menu/Rumble.button_pressed = Global.analog
 	$Options/Menu/Analog.button_pressed = Global.rumble
 
 
-func _on_controls_pressed():
+func _on_controls_pressed() -> void:
 	$Options/Controls.show()
 
-func _on_controls_back_pressed():
+func _on_controls_back_pressed() -> void:
 	$Options/Controls.hide()
 
-func _on_back_pressed():
+func _on_back_pressed() -> void:
 	hide()
-	emit_signal("closed")
+	closed.emit()
 
-func _on_sensativity_value_changed(value):
+func _on_sensativity_value_changed(value : float) -> void:
 	Global.sensativity = max(0.1,value)
 	$Options/Menu/Sensativity/SensBar.value = value
 
-func _input(event):
+func _input(event : InputEvent) -> void:
 	# close on pause pressed
 	if event.is_action_pressed("gm_pause") && visible && get_tree().paused:
 		_on_back_pressed()
 
-func _on_rumble_toggled(toggled_on):
+func _on_rumble_toggled(toggled_on : bool) -> void:
 	Global.rumble = toggled_on
 
-func _on_analog_toggled(toggled_on):
+func _on_analog_toggled(toggled_on : bool) -> void:
 	Global.analog = toggled_on
 
 
-func _on_to_menu_pressed():
+func _on_to_menu_pressed() -> void:
 	_on_back_pressed()
 	# Reset values when quitting game instead of when exiting options
 	Global.save_settings()
@@ -43,5 +43,5 @@ func _on_to_menu_pressed():
 	get_tree().change_scene_to_file("res://scenes/menu.tscn")
 
 # determine if to show the menu button if not in the menu scene (do a name check, if you change the main menu node name then you'll have to change this too)
-func _on_visibility_changed():
+func _on_visibility_changed() -> void:
 	$Options/ToMenu.visible = get_tree().current_scene.name != "Menu"
