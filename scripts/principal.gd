ec4a7e8d23f5ca25c57fb740cb10bb63b2ad7d62
me <terrype001@gmail.com>
2026-01-19 16:20:47 -0300

    Principal script now statically typed

--- scripts/principal.gd
+++ scripts/principal.gd
@@ -1,18 +1,18 @@
 extends Character
 class_name Principal
 
-@export var active = false
+@export var active : bool = false
 
-var seeRuleBreak = false
-var bullySeen = false
-var coolDown = 0.0
-var timeSeenRuleBreak = 0.0
-var angry = false
-var inOffice = false
-var detentions = 0
-var lockTimes = [15,30,45,60,99]
+var seeRuleBreak : bool = false
+var bullySeen : bool = false
+var coolDown : float = 0.0
+var timeSeenRuleBreak : float = 0.0
+var angry : bool = false
+var inOffice : bool = false
+var detentions : int = 0
+var lockTimes : Array[int] = [15,30,45,60,99]
 
-var audTimes = [
+var audTimes : Array[AudioStream] = [
 preload("res://audio/Characters/Principal/Times/PRI_15Sec.wav"),
 preload("res://audio/Characters/Principal/Times/PRI_30Sec.wav"),
 preload("res://audio/Characters/Principal/Times/PRI_45Sec.wav"),
@@ -20,42 +20,48 @@ preload("res://audio/Characters/Principal/Times/PRI_60Sec.wav"),
 preload("res://audio/Characters/Principal/Times/PRI_99Sec.wav"),
 ]
 
-var audScolds = [
+var audScolds : Array[AudioStream]= [
 preload("res://audio/Characters/Principal/Scolds/PRI_KnowBetter.wav"),
 preload("res://audio/Characters/Principal/Scolds/PRI_WhenLearn.wav"),
 preload("res://audio/Characters/Principal/Scolds/PRI_YourParents.wav"),
 ]
 
-var audDetention = preload("res://audio/Characters/Principal/PRI_DetentionForYou.wav")
-var audNoDrinking = preload("res://audio/Characters/Principal/RuleBroke/PRI_NoDrinking.wav")
-var audNoBullying = preload("res://audio/Characters/Principal/RuleBroke/PRI_NoBullying.wav")
-var audNoFaculty = preload("res://audio/Characters/Principal/RuleBroke/PRI_NoFaculty.wav")
-var audNoLockers = preload("res://audio/Characters/Principal/RuleBroke/Unused/PRI_NoLockers.wav")
-var audNoRunning = preload("res://audio/Characters/Principal/RuleBroke/PRI_NoRunning.wav")
-var audNoStabbing = preload("res://audio/Characters/Principal/RuleBroke/Unused/PRI_NoStabbing.wav")
-var audNoEscaping = preload("res://audio/Characters/Principal/RuleBroke/PRI_NoEscaping.wav")
-var audWhistle = preload("res://audio/Characters/Principal/PRI_Whistle.wav")
-var audDelay
+var audDetention : AudioStream = preload("res://audio/Characters/Principal/PRI_DetentionForYou.wav")
+var audNoDrinking : AudioStream = preload("res://audio/Characters/Principal/RuleBroke/PRI_NoDrinking.wav")
+var audNoBullying : AudioStream = preload("res://audio/Characters/Principal/RuleBroke/PRI_NoBullying.wav")
+var audNoFaculty : AudioStream = preload("res://audio/Characters/Principal/RuleBroke/PRI_NoFaculty.wav")
+var audNoLockers : AudioStream = preload("res://audio/Characters/Principal/RuleBroke/Unused/PRI_NoLockers.wav")
+var audNoRunning : AudioStream = preload("res://audio/Characters/Principal/RuleBroke/PRI_NoRunning.wav")
+var audNoStabbing : AudioStream = preload("res://audio/Characters/Principal/RuleBroke/Unused/PRI_NoStabbing.wav")
+var audNoEscaping : AudioStream = preload("res://audio/Characters/Principal/RuleBroke/PRI_NoEscaping.wav")
+var audWhistle : AudioStream = preload("res://audio/Characters/Principal/PRI_Whistle.wav")
+# Unused variable, always comment these when you find that a variable is not used
+# in any script or any other form, you can determine if a variable or function is unused
+# by searching the exact name of it on "Project" > "Find in files..." or by pressing
+# Cntrl + Shift + F as a shortcut, if you only want to search in this script only, press F3
+# or press in the script editor button "Search" > "Find"
+#var audDelay
 
-var aim = Vector3.ZERO
-var audioQueue = []
-@onready var playerChecker = $PlayerChecker
-var canSeePlayer = false
-@onready var sounds = $Sounds
+var aim := Vector3.ZERO
+var audioQueue : Array[AudioStream] = []
+@onready var playerChecker : RayCast3D = $PlayerChecker
+var canSeePlayer : bool = false
+@onready var sounds : AudioStreamPlayer3D = $Sounds
 
-@onready var principalOfficeLocation = global_position
+@onready var principalOfficeLocation : Vector3 = global_position
 
-func _ready():
+func _ready() -> void:
 	super()
 	set_physics_process(active)
 	visible = active
 
-func activate():
+func activate() -> void:
 	active = true
 	set_physics_process(active)
 	show()
 
-func _physics_process(delta): #I don't like the idea of game logic being tied to frame rate, process should be used for animation and audio related events
+#I don't like the idea of game logic being tied to frame rate, process should be used for animation and audio related events
+func _physics_process(delta : float) -> void: 
 	if seeRuleBreak:
 		timeSeenRuleBreak += delta
 		if timeSeenRuleBreak >= 0.5 && !angry:
@@ -100,21 +106,21 @@ func _physics_process(delta): #I don't like the idea of game logic being tied to
 	velocity.y = 0.0
 	
 
-func wander():
+func wander() -> void:
 	navAgent.target_position = Global.get_wander_point()# set random target based on targets
 	coolDown = 1.0
 	if randf_range(0.0,10.0) <= 1.0 && !sounds.playing:
 		sounds.stream = audWhistle
 		sounds.play()
 
-func queue_audio(audio:AudioStream = null):
+func queue_audio(audio : AudioStream = null) -> void:
 	audioQueue.append(audio)
 	if !sounds.playing:
 		sounds.stream = audioQueue[0]
 		sounds.play()
 		audioQueue.pop_front()
 
-func correct_player():
+func correct_player() -> void:
 	sounds.stop()
 	audioQueue.clear()
 	# get player rule break
@@ -129,7 +135,7 @@ func correct_player():
 			queue_audio(audNoRunning)
 
 ## Catching player
-func _on_player_collider_body_entered(body):
+func _on_player_collider_body_entered(body : Node3D) -> void:
 	if body is Player && angry && !inOffice:
 		inOffice = true
 		global_position = principalOfficeLocation+Vector3(0,0,-10)
@@ -152,17 +158,15 @@ func _on_player_collider_body_entered(body):
 		queue_audio(audDetention)
 		queue_audio(audScolds[randi_range(0,audScolds.size()-1)])
 		detentions = min(detentions+1,4)
-	
-	
 
 
-func _on_sounds_finished():# queue next audio
+func _on_sounds_finished() -> void: # queue next audio
 	if audioQueue.size() > 0:
 		sounds.stream = audioQueue[0]
 		sounds.play()
 		audioQueue.pop_front()
 
-func target_bully():
+func target_bully() -> void:
 	if !bullySeen:
 		navAgent.target_position = Global.bully.global_position
 		queue_audio(audNoBullying)
