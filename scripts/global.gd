490189ce8153ec71cd6fc4b097c5f57f6b8b4462
me <terrype001@gmail.com>
2026-01-19 18:58:25 -0300

    Global script now statically typed

--- scripts/global.gd
+++ scripts/global.gd
@@ -1,6 +1,7 @@
 extends Node
 
-var MathGame = preload("res://entities/math_game.tscn")
+signal note_books_updated
+var MathGame : PackedScene = preload("res://entities/math_game.tscn")
 
 var player : Player = null
 var bully : Bully = null
@@ -8,11 +9,11 @@ var baldi : Baldi = null
 var crafters : Crafters = null
 var background : WorldEnvironment = null
 
-var endless = false
-var secret = true # secret exit for if you fail all note books
+var endless : bool = false
+var secret : bool = true # secret exit for if you fail all note books
 
 enum ITEMS {NONE, ZESTI, LOCK, KEY, BSODA, QUARTER, TAPE, ALARM, NO_SQUEE, SCISSORS, BOOTS}
-var itemTextures = [
+var itemTextures : Array[Texture2D] = [
 null,
 preload("res://graphics/SchoolHouse/PickUps/EnergyFlavoredZestyBar.png"),
 preload("res://graphics/SchoolHouse/PickUps/YellowDoorLock.png"),
@@ -26,40 +27,38 @@ preload("res://graphics/SchoolHouse/PickUps/SafetyScissors.png"),
 preload("res://graphics/SchoolHouse/PickUps/BootsIcon.png")
 ]
 
-signal note_books_updated
+var audMachineQuite : AudioStream = preload("res://audio/SFX/FinalMode/quiet noise loop.wav")
+var audMachineStart : AudioStream = preload("res://audio/SFX/FinalMode/LoudNoiseIntroLoop.wav")
+var audMachineRev : AudioStream = preload("res://audio/SFX/FinalMode/Loud noise 3.wav")
 
-var audMachineQuite = preload("res://audio/SFX/FinalMode/quiet noise loop.wav")
-var audMachineStart = preload("res://audio/SFX/FinalMode/LoudNoiseIntroLoop.wav")
-var audMachineRev = preload("res://audio/SFX/FinalMode/Loud noise 3.wav")
-
-var noteBooks = 0:
+var noteBooks : int = 0:
 	get:
 		return noteBooks
 	set(value):
 		noteBooks = value
-		emit_signal("note_books_updated")
-var faildBooks = 0
+		note_books_updated.emit()
+var faildBooks : int = 0
 
-var spoopMode = false
+var spoopMode : bool = false
 
-var escapeMode = false
-var escapesReached = 0
+var escapeMode : bool = false
+var escapesReached : int = 0
 
 #region options
-var sensativity = 20.0
-var analog = true
-var rumble = true
+var sensativity : float = 20.0
+var analog : bool = true
+var rumble : bool = true
 #endregion
 
-func get_wander_point(group = "wander", min_range = 0, max_range = 99999) -> Vector3:
-	var wanderPoints = get_tree().get_nodes_in_group(group)
-	var getPoint = wanderPoints[randi_range(min_range,min(wanderPoints.size()-1,max_range))].global_position
-	for i in get_tree().get_nodes_in_group("ambience"):
+func get_wander_point(group : StringName = "wander", min_range : int = 0, max_range : int = 99999) -> Vector3:
+	var wanderPoints : Array[Node] = get_tree().get_nodes_in_group(group)
+	var getPoint : Vector3 = wanderPoints[randi_range(min_range,min(wanderPoints.size()-1,max_range))].global_position
+	for i : Node in get_tree().get_nodes_in_group("ambience"):
 		if i is Ambience:
 			i.play_ambience(getPoint)
 	return getPoint # set target
 
-func reset_values():
+func reset_values() -> void:
 	noteBooks = 0
 	player = null
 	baldi = null
@@ -69,52 +68,51 @@ func reset_values():
 	secret = true
 	escapesReached = 0
 
-func exit_reached():
+func exit_reached() -> void:
 	# if you're looking for the exit level routine it's handled in the door script
 	escapesReached += 1
 	match(int(escapesReached)):
 		1: # first exit
 			# set scene to red
-			for i in get_tree().get_nodes_in_group("first_exit_trigger"):
+			for i : Node in get_tree().get_nodes_in_group("first_exit_trigger"):
 				if i is WorldEnvironment:
 					i.environment.ambient_light_color = Color.RED
 				elif i is AudioStreamPlayer: # play machine noise
 					i.stream = audMachineQuite
 					i.play()
 		2: # second exit
-			for i in get_tree().get_nodes_in_group("second_exit_trigger"):
+			for i : Node in get_tree().get_nodes_in_group("second_exit_trigger"):
 				if i is AudioStreamPlayer: # play machine noise
 					i.volume_db = -20.0 # lower volume so that you don't blow up someone's speakers
 					i.stream = audMachineStart
 					i.play()
 		3: # third exit
-			for i in get_tree().get_nodes_in_group("third_exit_trigger"):
+			for i : Node in get_tree().get_nodes_in_group("third_exit_trigger"):
 				if i is AudioStreamPlayer: # play machine noise
 					i.volume_db = -20.0 # lower volume so that you don't blow up someone's speakers
 					i.stream = audMachineRev
 					i.play()
 
-func lock_mouse():
+func lock_mouse() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
-func unlock_mouse():
+func unlock_mouse() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
-func save_settings():
+func save_settings() -> void:
 	# Create new ConfigFile object.
-	var config = ConfigFile.new()
+	var config := ConfigFile.new()
 	config.set_value("settings", "sensativity", sensativity)
 	config.set_value("settings", "analog", analog)
 	config.set_value("settings", "rumble", rumble)
 	# Save it to a file (overwrite if already exists).
 	config.save("user://settings.cfg")
 
-func load_settings():
-	var config = ConfigFile.new()
-
+func load_settings() -> void:
+	var config := ConfigFile.new()
 	# Load data from a file.
-	var err = config.load("user://settings.cfg")
-
+	var err : Error = config.load("user://settings.cfg")
+	
 	# If the file didn't load, ignore it.
 	if err != OK:
 		return
@@ -122,4 +120,3 @@ func load_settings():
 	sensativity = config.get_value("settings", "sensativity")
 	analog = config.get_value("settings", "analog")
 	rumble = config.get_value("settings", "rumble")
-	
