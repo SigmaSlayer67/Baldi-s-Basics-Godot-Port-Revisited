001d14fa041c61da13afde380c8e203a943e21cb
me <terrype001@gmail.com>
2026-02-14 22:24:56 -0300

    First commit for style guide, progress on Arts And Crafters script and importing of files also new project name

--- entities/characters/arts_and_crafters.gd
+++ entities/characters/arts_and_crafters.gd
@@ -1,17 +1,17 @@
 # ATTENTION: Script done! (check message that says: the script is fully statically typed and ready for execution, shall be removed when moving on to the next branch)
-extends Character
 class_name Crafters
+extends Character
 
-@export var active := false
+@export var active : bool = false
 
-@export var audCrafterLoop : AudioStream = preload("res://audio/Characters/ArtsAndCrafters/CFT_Loop.wav")
+@export var aud_crafter_loop : AudioStream = preload("res://audio/Characters/ArtsAndCrafters/CFT_Loop.wav")
 
-@export var angrySprite : Texture2D = preload("res://graphics/Characters/ArtsAndCrafters/Crafters_Ohno.png")
+@export var angry_sprite : Texture2D = preload("res://graphics/Characters/ArtsAndCrafters/Crafters_Ohno.png")
 
-@export var noteBookAnger : int = 7
+@export var note_book_anger : int = 7
 
-var angry := false
-var getting_angry := false
+var angry : bool = false
+var getting_angry : bool = false
 var anger : float = 0.0
 var force_show_time : float = 0.0
 
@@ -27,27 +27,30 @@ func _ready() -> void:
 	set_physics_process(active)
 	visible = active
 
+
 func activate() -> void:
 	active = true
 	set_physics_process(active)
 	show()
 
+
 func _process(delta : float) -> void:
-	force_show_time = move_toward(force_show_time,0.0,delta)
+	force_show_time = move_toward(force_show_time, 0.0, delta)
 	if getting_angry: # if arts is getting angry
 		anger += delta # Increase anger
-		if anger >= 1.0 && !angry: # If anger is greater then 1 and arts isn't angry
+		if anger >= 1.0 and not angry: # If anger is greater then 1 and arts isn't angry
 			angry = true # Get angry
 			sounds.play() # scream
-			sprite.texture = angrySprite
+			sprite.texture = angry_sprite
 	# if anger is greater then 0, decrease
 	elif anger > 0.0: 
-		anger = move_toward(anger,0.0,delta)
+		anger = move_toward(anger, 0.0, delta)
+
 
 func _physics_process(delta : float) -> void:
-	if !angry: # if not angry
+	if not angry: # if not angry
 		if is_instance_valid(Global.player):
-			if (global_position.distance_to(navAgent.get_final_position()) <= 20.0 && global_position.distance_to(Global.player.global_position) >= 60.0) || forceShowTime > 0.0: # if close to the player and force showtime is less then 0
+			if (global_position.distance_to(navAgent.get_final_position()) <= 20.0 and global_position.distance_to(Global.player.global_position) >= 60.0) or force_show_time > 0.0: # if close to the player and force showtime is less then 0
 				visible = true # show
 			else:
 				visible = false # hide
@@ -55,29 +58,29 @@ func _physics_process(delta : float) -> void:
 		speed += 60.0 * delta # increase the speed
 		navAgent.target_position = Global.player.global_position
 	
-	if Global.noteBooks >= noteBookAnger: # If the player has more then the note book count 
+	if Global.noteBooks >= note_book_anger: # If the player has more then the note book count 
 		player_checker.target_position = (Global.player.global_position - global_position).slide(Vector3.UP)
 		player_checker.force_raycast_update()
-		if !player_checker.is_colliding() && visibility_checker.is_on_screen() && visible: # if Arts is visible, and active and sees player
+		if !player_checker.is_colliding() and visibility_checker.is_on_screen() and visible: # if Arts is visible, and active and sees player
 			getting_angry = true # start getting angry
 		else:
 			getting_angry = false # stop being angry
 	super(delta)
 
+
 func give_location(location : Vector3, flee : bool) -> void:
-	if !angry && active:
+	if !angry and active:
 		navAgent.target_position = location
 		player_checker.target_position = (Global.player.global_position - global_position).slide(Vector3.UP)
 		player_checker.force_raycast_update()
-		if flee && !player_checker.is_colliding(): # show if fleeing and line of sight isn't broken
+		if flee and !player_checker.is_colliding(): # show if fleeing and line of sight isn't broken
 			force_show_time = 3.0 # Make arts appear in 3 seconds
 
 
 # play full whoosh sound if rotating
 func _on_sounds_finished() -> void:
-	sounds.stream = audCrafterLoop
+	sounds.stream = aud_crafter_loop
 	sounds.play()
-
 
 
 func _on_player_collider_body_entered(body : Node3D) -> void:
